ca394ebdfe67254be8bfb8c61742f64435ada1bb
serghku <sergei.kustsnko@gmail.com>
2018-09-24 19:42:21 +0300

    Javascript products page

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -1,9 +1,43 @@
 
-function showVacPouch() {
-    var elements2 = document.getElementsByClassName("plas_bag");
-    for (var i =0; i < elements2.length; i++) {
-        elements2[i].style.display = "none";
-        console.log(elements2[i]);
+function vacuumPouches() {
+    var elements1 = document.getElementsByClassName("vac_pouc"); //Elements which will be shown to user
+    var elements2 = document.getElementsByClassName("plastic_bag"); //Elements which will be hidden
+    var elements3 = document.getElementsByClassName("pack_material"); //Elements which will be hidden
+
+    showLoop(elements1);
+    hideLoop(elements2);
+    hideLoop(elements3);
+}
+
+function plasticBags(){
+    var elements1 = document.getElementsByClassName("plastic_bag"); //Elements which will be shown to user
+    var elements2 = document.getElementsByClassName("vac_pouc"); //Elements which will be hidden
+    var elements3 = document.getElementsByClassName("pack_material"); //Elements which will be hidden
+
+    showLoop(elements1);
+    hideLoop(elements2);
+    hideLoop(elements3);
+}
+
+function packMaterial(){
+    var elements1 = document.getElementsByClassName("pack_material"); //Elements which will be shown to user
+    var elements2 = document.getElementsByClassName("plastic_bag"); //Elements which will be hidden
+    var elements3 = document.getElementsByClassName("vac_pouc"); //Elements which will be hidden
+
+    showLoop(elements1);
+    hideLoop(elements2);
+    hideLoop(elements3);
+}
+
+function hideLoop(elements) {
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+}
+
+function showLoop(elements){
+    for (var i = 0; i<elements.length; i++){
+        elements[i].style.display = "inline-block";
     }
 }
 
