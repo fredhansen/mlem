d2cd8c2f4c5364d758cf65005303563884684a24
@serghku <sergei.kustsenko@gmail.com>
2018-11-12 18:27:26 +0200

    Gmail password

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,5 +1,6 @@
 package application;
 
+import application.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
