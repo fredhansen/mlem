fc4f65317326a83a7e9ada4fd6a2b7a3da523549
serghku <sergei.kustsenko@gmail.com>
2018-11-30 21:44:16 +0200

    CSS and JS optimization

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,13 +1,1 @@
-var mymap = L.map('mapid').setView([58.378691,26.694792], 15.7);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-}).addTo(mymap);
-
-L.marker([58.378691,26.694792]).addTo(mymap)
-    .bindPopup('Welding Group')
-    .openPopup();
+var mymap=L.map('mapid').setView([58.378691,26.694792],15.7);L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',{maxZoom:18,attribution:'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, '+'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, '+'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',id:'mapbox.streets'}).addTo(mymap);L.marker([58.378691,26.694792]).addTo(mymap).bindPopup('Welding Group').openPopup()
