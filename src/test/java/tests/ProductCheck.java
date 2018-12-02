da33d778d658af8b2fc73108f164824ab7bc0b5a
serghku <sergei.kustsenko@gmail.com>
2018-12-02 15:58:30 +0200

    test fix

--- src/test/java/tests/ProductCheck.java
+++ src/test/java/tests/ProductCheck.java
@@ -31,11 +31,11 @@ public class ProductCheck {
     private final static String url = "http://localhost:8080/";
 
     @Before
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir")+
-                        File.separator +"src"+
-                        File.separator + "main"+
+                System.getProperty("user.dir") +
+                        File.separator + "src" +
+                        File.separator + "main" +
                         File.separator + "resources" +
                         File.separator + "selenium" +
                         File.separator + "chromedriver.exe");
@@ -44,13 +44,14 @@ public class ProductCheck {
     }
 
     @After
-    public void quitDriver(){
+    public void quitDriver() {
         driver.quit();
     }
 
     @Test
-    public void Add_Product_And_Category_Then_Check_In_Warehouse() throws IOException, JSONException {
+    public void Add_Product_And_Category_Then_Check_In_Warehouse() throws IOException, JSONException, InterruptedException {
         NavigationTest.login_With_Google(this.driver);
+
         goToPage("products/add");
         //Creating random product and category
         Category category = new CategoryCreator().create();
@@ -63,18 +64,19 @@ public class ProductCheck {
         System.out.println(categoryName_In_DB);
 
         //Adding product to warehouse
-        add_Product_To_Warehouse(product,category);
+        add_Product_To_Warehouse(product, category);
+
         //Getting response from ajax request
         String productId_In_DB = get_Product_Response_In_JsonObject().get("id").toString();
         String categoryId_In_DB = get_Product_Response_In_JsonObject().get("categoryId").toString();
-        System.out.println("Product ID in database: " +productId_In_DB);
-        System.out.println("Category ID in database: "+categoryId_In_DB);
+        System.out.println("Product ID in database: " + productId_In_DB);
+        System.out.println("Category ID in database: " + categoryId_In_DB);
 
         //Searching product and checking id in warehouse
         search_Product_In_Warehouse_By_Id(productId_In_DB);
 
         //Searching category id in results
-        Assert.assertTrue(driver.findElement(By.xpath("//*[text()[contains(.,'"+categoryId_In_DB+ "')]]")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()[contains(.,'" + categoryId_In_DB + "')]]")).isDisplayed());
         WebElement productDeleteHref = driver.findElementById(productId_In_DB);
         Assert.assertTrue(productDeleteHref.isDisplayed());
         productDeleteHref.click();
@@ -85,27 +87,23 @@ public class ProductCheck {
         quitDriver();
     }
 
-    private void add_Category_To_Warehouse(Category category){
+    private void add_Category_To_Warehouse(Category category) {
         WebElement categoryInput = driver.findElementById("cat_name");
         categoryInput.sendKeys(category.getName());
         driver.findElementById("submitCategory").click();
     }
 
-    private JSONObject get_Category_Response_In_JsonObject() throws JSONException {
-        WebDriverWait wait = new WebDriverWait(driver,25);
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElementById("getCategoryStatus")));
+    private JSONObject get_Category_Response_In_JsonObject() throws JSONException, InterruptedException {
         WebElement categoryAjaxResponse = driver.findElementById("getCategoryStatus");
         return new JSONObject(categoryAjaxResponse.getText());
     }
 
-    private JSONObject get_Product_Response_In_JsonObject() throws JSONException {
-        WebDriverWait wait = new WebDriverWait(driver,25);
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElementById("getAddingStatus")));
+    private JSONObject get_Product_Response_In_JsonObject() throws JSONException, InterruptedException {
         WebElement productAjaxResponse = driver.findElementById("getAddingStatus");
         return new JSONObject(productAjaxResponse.getText());
     }
 
-    private void add_Product_To_Warehouse(Product product, Category category){
+    private void add_Product_To_Warehouse(Product product, Category category) {
         driver.findElementById("name").sendKeys(product.getName());
         driver.findElementById("tag").sendKeys(product.getTag());
         driver.findElementById("description").sendKeys(product.getDescription());
@@ -119,21 +117,31 @@ public class ProductCheck {
         driver.findElementById("addButton").click();
     }
 
-    private void search_Category_In_Warehouse(Category category){
+    private void search_Category_In_Warehouse(Category category) {
         goToPage("products/warehouse");
+        WebDriverWait wait = new WebDriverWait(this.driver, 25);
+        wait.until(ExpectedConditions.visibilityOfAllElements(
+                driver.findElementById("categoryRadio"),
+                driver.findElementById("categoryRadio"),
+                driver.findElementById("searchButton")));
         driver.findElementById("categoryRadio").click();
-        driver.findElementById("searchInput").sendKeys(category.getName());
+        driver.findElementById("categoryRadio").sendKeys(category.getName());
         driver.findElementById("searchButton").click();
     }
 
-    private void search_Product_In_Warehouse_By_Id(String productId){
+    private void search_Product_In_Warehouse_By_Id(String productId) {
         goToPage("products/warehouse");
+        WebDriverWait wait = new WebDriverWait(this.driver, 25);
+        wait.until(ExpectedConditions.visibilityOfAllElements(
+                driver.findElementById("categoryRadio"),
+                driver.findElementById("categoryRadio"),
+                driver.findElementById("searchButton")));
         driver.findElementById("productsRadio").click();
         driver.findElementById("searchInput").sendKeys(productId);
         driver.findElementById("searchButton").click();
     }
-    
-    private void goToPage(String page){
-        driver.get(url +page);
+
+    private void goToPage(String page) {
+        driver.get(url + page);
     }
 }
