b2c3ff9df97cae9d3b1087d6a0a67dee1860d10e
Helena <h@asf.com>
2018-12-10 17:11:21 +0200

    styling

--- src/main/resources/static/js/productDetail.js
+++ src/main/resources/static/js/productDetail.js
@@ -1,12 +1,32 @@
-$(document).ready(function() {
-    $("#submitToCart").submit(function(event) {
-        event.preventDefault();
-        ajaxPostToCart()
-    });
+var check = false;
 
+$(document).ready(function () {
+    $(".btn").click(function () {
+        check = true;
+        event.preventDefault();
+        ajaxPostToCart();
+    });
 });
 
 function ajaxPostToCart() {
 
-/*"/cart/add/{id}"*/
+   // "/cart/add/{id}"
 }
+
+//BUTTONS
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+}
+
